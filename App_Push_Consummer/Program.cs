788af74637d2718d385b77759a8a6b827bc94aba
lecuong <kuonglvstudio@gmail.com>
2024-07-25 13:03:10 +0700

    cm3

--- App_Push_Consummer/Program.cs
+++ App_Push_Consummer/Program.cs
@@ -15,7 +15,7 @@ namespace App_Push_Consummer
     /// <summary>
     /// App: Xử lý các tiến trình được push về từ FRONT END
     /// /// </summary>
-    /// 
+    
     class Program
     {
         private static string queue_checkout_order = ConfigurationManager.AppSettings["queue_name"];
