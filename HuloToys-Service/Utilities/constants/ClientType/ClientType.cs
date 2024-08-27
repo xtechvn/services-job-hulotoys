7397ed1109f3e25a7f0db4fb8d68520e738684b6
Hiếu <anhhieuk50@gmail.com>
2024-08-27 17:06:52 +0700

    update ClientType

--- HuloToys-Service/Utilities/constants/ClientType/ClientType.cs
+++ HuloToys-Service/Utilities/constants/ClientType/ClientType.cs
@@ -2,15 +2,11 @@
 {
     public enum ClientType
     {
-        ALL = -1,
-        AGENT = 1, // Đại lý
-        TIER_1_AGENT = 2, // Đối tác chiến lược
-        TIER_2_AGENT = 3, // Đại lý cấp 2
-        TIER_3_AGENT = 4,// Đại lý cấp 3
-        CUSTOMER = 5, //Khách lẻ
-        SALE = 6, // nv kinh doanh
-        ENTERPRISE = 7, // Doanh nghiệp
-        COLLABORATORS = 8// Cộng tác viên
+        KL = 0,
+        DOI_TAC_CL = 1,
+        CTV = 2,
+        DAILYC1 = 3,
+        DOANH_NGHIEP = 4,
 
     }
     public enum ClientTypes
@@ -34,5 +30,5 @@
         GUEST_CHILD = 2, // Thông tin thành viên trong đoàn là người lớn
         GUEST_INFANT = 2, // Thông tin thành viên trong đoàn là trẻ sơ sinh
     }
- 
+
 }
--- HuloToys-Service/Utilities/constants/ClientType/ClientTypeName.cs
+++ HuloToys-Service/Utilities/constants/ClientType/ClientTypeName.cs
@@ -4,14 +4,11 @@
     {
         public static readonly Dictionary<Int16, string> service = new Dictionary<Int16, string>
         {
-            {Convert.ToInt16(ClientType.AGENT), "DL" },
-            {Convert.ToInt16(ClientType.TIER_1_AGENT), "CL" },
-            {Convert.ToInt16(ClientType.TIER_2_AGENT), "DL" },
-            {Convert.ToInt16(ClientType.TIER_3_AGENT), "DL" },
-            {Convert.ToInt16(ClientType.CUSTOMER), "KL" },
-            {Convert.ToInt16(ClientType.SALE), "SL" },
-            {Convert.ToInt16(ClientType.ENTERPRISE), "DN" },
-            {Convert.ToInt16(ClientType.COLLABORATORS), "CT" }
+            {Convert.ToInt16(ClientType.DAILYC1), "DL" },
+            {Convert.ToInt16(ClientType.KL), "KL" },
+            {Convert.ToInt16(ClientType.DOI_TAC_CL), "CL" },
+            {Convert.ToInt16(ClientType.DOANH_NGHIEP), "DN" },
+            {Convert.ToInt16(ClientType.CTV), "CTV" }
         };
     }
 }
