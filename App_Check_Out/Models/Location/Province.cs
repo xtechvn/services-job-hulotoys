c6fd34e2954a280eb49d7689fc4593146a602c32
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-09-09 15:54:34 +0700

    fix model mongodb

---
+++ App_Check_Out/Models/Location/Province.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuloToys_Service.Models.Location;
+
+public class Province
+{
+    public int Id { get; set; }
+
+    public string ProvinceId { get; set; }
+
+    public string Name { get; set; }
+
+    public string NameNonUnicode { get; set; }
+
+    public string Type { get; set; }
+
+    public short? Status { get; set; }
+}
---
+++ App_Check_Out/Models/Orders/AddressClientFEModel.cs
@@ -0,0 +1,16 @@
+﻿using HuloToys_Service.Models.Location;
+using System;
+using System.Collections.Generic;
+
+namespace Entities.Models
+{
+    public partial class AddressClientFEModel : AddressClientESModel
+    {
+      public Province province_detail { get; set; }
+      public District district_detail { get; set; }
+      public Ward ward_detail { get; set; }
+
+    }
+
+
+}
